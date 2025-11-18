d95da5dca46ea55525bfc6bf70775543113ca97d
Pratyush Rahul Ranjan <ranjan.pratyush24@gmail.com>
2025-11-18 11:45:08 +0530

    lol

---
+++ A_Array_Coloring.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+        sum += v[i];
+    }
+    if(sum%2==0){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
