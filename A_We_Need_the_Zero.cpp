dd4a36f1ba80e9a25720d8d03e135c954709426e
Pratyush Rahul Ranjan <ranjan.pratyush24@gmail.com>
2025-11-22 11:26:45 +0530

    lol

--- A_We_Need_the_Zero.cpp
+++ A_We_Need_the_Zero.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 void solve(){
-    
+    int n;
+    cin >> n;
+    vector<int>v(n);
+    int x = 0;
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+        x = x ^ v[i];
+    }
+
+    if(n%2==0){
+        if(x==0){
+            cout << 0 << endl;
+        }else{
+            cout << -1 << endl;
+        }
+    }else{
+        cout << x << endl;
+    }
 }
 
 int main(){
